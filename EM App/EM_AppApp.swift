ea72674ef98095a888952e92bee6694a4a6fbf02
RealWarrior03 <henry.krieger@me.com>
2021-01-12 22:20:02 +0100

    Initial Commit

---
+++ EM App/EM_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  EM_AppApp.swift
+//  EM App
+//
+//  Created by Henry Krieger on 12.01.21.
+//
+
+import SwiftUI
+
+@main
+struct EM_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
