4a434ef3c442f1ae4a5b1e132aa52280bc9eb99b
RealWarrior03 <henry.krieger@me.com>
2021-01-19 17:18:14 +0100

    Testing Area for short time

--- EM App/ContentView.swift
+++ EM App/ContentView.swift
@@ -9,38 +9,18 @@ import SwiftUI
 //import CoreData
 
 struct ContentView: View {
-    @State private var selection: Tab = .home
-    
-    enum Tab {
-        case home
-        case news
-        case settings
-    }
-
     var body: some View {
-        TabView(selection: $selection) {
-            home()
-                .tabItem {
-                    Label("Home", systemImage: "house")
-                }
-                .tag(Tab.home)
-            news()
-                .tabItem {
-                    Label("News", systemImage: "tray")
-                }
-                .tag(Tab.news)
-            settings()
-                .tabItem {
-                    Label("Settings", systemImage: "gear")
-                }
-                .tag(Tab.settings)
-        }
+        login()
     }
 }
 
+class SourceOfTruth: ObservableObject {
+    @Published var count = 0
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(SourceOfTruth())
     }
 }
