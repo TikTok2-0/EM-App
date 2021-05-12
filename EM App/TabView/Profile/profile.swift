fff4fe0274cb30de757e050f4ee421fea091db8c
RealWarrior03 <henry.krieger@me.com>
2021-05-12 21:41:42 +0200

    Privacy Policy

--- EM App/TabView/Profile/profile.swift
+++ EM App/TabView/Profile/profile.swift
@@ -124,6 +124,9 @@ struct profile: View {
                     NavigationLink(destination: aboutus()) {
                         Label("About Us", systemImage: "info")
                     }
+                    NavigationLink(destination: PrivacyPolicy()) {
+                        Label("Privacy Policy", systemImage: "hand.raised")
+                    }
                 }
             }
             .navigationTitle("Profile")
@@ -138,6 +141,115 @@ struct profile: View {
                 settings()
             })
         }
+    }
+}
+
+struct PrivacyPolicy: View {
+    @State var showSafari: Bool = false
+    
+    var body: some View {
+        List {
+            Button(action: { showSafari.toggle() }) {
+                Label("Privacy Policy", systemImage: "link").fullScreenCover(isPresented: $showSafari) {
+                    SafariView(url: URL(string: "https://www.privacypolicies.com/live/a3287839-794c-4c4a-b883-f18a6e586f6f")!).ignoresSafeArea(edges: .all)
+                }
+            }
+            
+            Text("All data is only stored locally on your device and we are not able to view any of that data.")
+            Text("Exception: If you give feedback you consent to sharing your email address with us")
+            
+            Section(header: Text("Contact Info")) {
+                VStack(alignment: .leading) {
+                    Text("Name")
+                        .fontWeight(.bold)
+                    Text("Only locally (First Name, Last Name)")
+                }
+                VStack(alignment: .leading) {
+                    Text("Email Address")
+                        .fontWeight(.bold)
+                    Text("Only locally (E-Mail)")
+                    Text("Exception: Feedback")
+                }
+                VStack(alignment: .leading) {
+                    Text("Phone Number")
+                        .fontWeight(.bold)
+                    Text("-")
+                }
+                VStack(alignment: .leading) {
+                    Text("Physical Address")
+                        .fontWeight(.bold)
+                    Text("-")
+                }
+                VStack(alignment: .leading) {
+                    Text("Other User Contact Info")
+                        .fontWeight(.bold)
+                    Text("Only locally (Username, School, Role, Class)")
+                }
+            }
+            Section(header: Text("User Content")) {
+                VStack(alignment: .leading) {
+                    Text("Emails or Text Messages")
+                        .fontWeight(.bold)
+                    Text("-")
+                }
+                VStack(alignment: .leading) {
+                    Text("Photos or Videos")
+                        .fontWeight(.bold)
+                    Text("-")
+                }
+                VStack(alignment: .leading) {
+                    Text("Audio Data")
+                        .fontWeight(.bold)
+                    Text("-")
+                }
+                VStack(alignment: .leading) {
+                    Text("Gameplay Content")
+                        .fontWeight(.bold)
+                    Text("-")
+                }
+                VStack(alignment: .leading) {
+                    Text("Customer Support")
+                        .fontWeight(.bold)
+                    Text("When you give feedback we get access to your email address.")
+                }
+                VStack(alignment: .leading) {
+                    Text("Other User Content")
+                        .fontWeight(.bold)
+                    Text("-")
+                }
+            }
+            Section(header: Text("Identifiers")) {
+                VStack(alignment: .leading) {
+                    Text("User ID")
+                        .fontWeight(.bold)
+                    Text("-")
+                }
+                VStack(alignment: .leading) {
+                    Text("Device ID")
+                        .fontWeight(.bold)
+                    Text("-")
+                }
+            }
+            Section(header: Text("Diagnostics")) {
+                VStack(alignment: .leading) {
+                    Text("Crash Data")
+                        .fontWeight(.bold)
+                    Text("Shared with us within beta phase for debugging")
+                }
+                VStack(alignment: .leading) {
+                    Text("Performance Data")
+                        .fontWeight(.bold)
+                    Text("-")
+                }
+                VStack(alignment: .leading) {
+                    Text("Other Diagnostic Data")
+                        .fontWeight(.bold)
+                    Text("-")
+                }
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
+        .navigationTitle("Privacy Policy")
     }
 }
 
